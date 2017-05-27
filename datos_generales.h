4cf9867d0e3ca412a43ca72d292fffe949f25af3
ignacio <ignacio@Ignacio>
2017-05-27 01:40:05 -0300

    Datos generales para el TP

---
+++ datos_generales.h
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   datos_generales.h
+ * Author: ignacio
+ *
+ * Created on 26 de mayo de 2017, 20:28
+ */
+
+#ifndef DATOS_GENERALES_H
+#define DATOS_GENERALES_H
+
+#define ANCHO_DIS   640
+#define ALTO_DIS    480
+
+#define CANT_PUERTOS_AUX 2
+#define CANT_LEDS_PUERTO 8
+#define CANT_LEDS_D 2*CANT_LEDS_PUERTO
+
+
+
+#define APAGADO "red"
+#define PRENDIDO "green"
+
+
+#endif /* DATOS_GENERALES_H */
+
