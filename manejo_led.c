249f976daf04d40dfc6e77a31b86ba78ead06021
ignacio <ignacio@Ignacio>
2017-05-28 20:34:08 -0300

    1) Borre las funciones crear_led y crearpuertoD (esta misma la remplace con la funcion que ya existia, la cual es crear modificar_estado_leds) 2) En la funcion modificar_estado_leds no creo mas punteros a bitmaps, los recibo como argumento (esos son correspondiente al fondo "atras" de los leds, el led prendido a dibujar y el led apagado)

--- manejo_led.c
+++ manejo_led.c
@@ -13,21 +13,11 @@
 #include "manejo_led.h"
 #include "manejo_estruct.h"
 
-#define ANCHO_LED  25
-#define ALTO_LED    50
-
 
 #define SEPA_ENTRE_LEDS 10
 #define SEPA_PUERTOS_AUX 50
 
 //PROTOTIPOS//
-
-
-bool crear_led (ALLEGRO_DISPLAY *display, ALLEGRO_BITMAP *led,float ancho, float alto, float posx, float posy);
-//se crea un LED APAGADO
-//devuelve un bool en que si es true, significa que se pudo crear el led correctamente; caso contrario, fallo
-//al crear el led
-//(necesito como argumento el display donde va a ser creado el led)
 
 
 float calc_posx_led (int nro_led, int tamano_puerto);
@@ -43,170 +33,46 @@ float calc_posy_led (int nro_puerto_aux, int tamano_puerto);
 
 //DEFINICION FUNCIONES//
 
-bool crear_puertoD(ALLEGRO_DISPLAY *display, ALLEGRO_BITMAP * puerto[])
+
+void cambiar_estado_leds (ALLEGRO_DISPLAY *display, ALLEGRO_BITMAP * led_apagado, 
+        ALLEGRO_BITMAP * led_prendido, dos_byte_t datos, ALLEGRO_BITMAP * fondo)
 {
-    bool control = true;
-    
-    int cont_puertos;
-    int cont_leds;
-    
-    for (cont_puertos=0;cont_puertos<CANT_PUERTOS_AUX;cont_puertos++) 
-    //dibujo el puerto total en el display
-    //en el caso mas basico en el que haya dos puertos (CANT_PUERTO_AUX=2),
-    // cuando cont_puertos = 0, creo en el display la cantidad de leds 
-    //correspondiente (CANT_LEDS_PUERTO) del puerto B y cuando sea
-    //cont_puertos = 1 idem lo anterior pero del puerto A.
-    //Las consideraciones que se toman para ver si el puerto que estoy creando
-    //en pantalla es menor a la mitad de la cantidad total es para centrar
-    //al puerto total en el centro del display.
-    {
-        float posy = calc_posy_led (cont_puertos, CANT_PUERTOS_AUX);
-        
-        for(cont_leds = 0; cont_leds < CANT_LEDS_PUERTO; cont_leds++) //creo en pantalla al puerto B
-        {
-        //dibujo un puerto auxiliar (ejemplo el puerto A) en el display
-        //en el caso mas basico en el que haya dos Leds en el puerto (CANT_LEDS_PUERTO=2),
-        //creo la cantidad de leds que me determina el define CANT_LEDS_PUERTO
-        //las consideraciones que se realizan es para que el puerto que se esta creando
-        //este centrado en el ancho del display
-        
-            float posx = calc_posx_led (cont_leds, CANT_LEDS_PUERTO);
-
-
-            control = crear_led(display,puerto[cont_leds+cont_puertos*CANT_LEDS_PUERTO],
-                    ANCHO_LED,ALTO_LED,posx,posy);
-            
-            if (control == false)
-            {
-                cont_leds = CANT_LEDS_PUERTO;
-                cont_puertos = CANT_PUERTOS_AUX; 
-            }
-        }
-
-    }
-    
-    return control;
-}
-
-
-
-
-
-
-
-bool cambiar_estado_leds (ALLEGRO_DISPLAY *display, ALLEGRO_BITMAP * puerto [], dos_byte_t datos)
-{
-           
-   
-    
-    bool control = true;
-    
     char estado;
     
     int cont_puerto;
     int cont_leds;
+     
+    al_set_target_bitmap(led_prendido);
+    al_clear_to_color(al_color_name(PRENDIDO));
     
+    al_set_target_bitmap(led_apagado);
+    al_clear_to_color(al_color_name(APAGADO));
     
-    ALLEGRO_BITMAP *prendido = NULL;
-    ALLEGRO_BITMAP *apagado = NULL;
+    al_set_target_backbuffer(display);
+    al_draw_bitmap(fondo,0,0,0);     
     
-    prendido = al_create_bitmap(ANCHO_LED,ALTO_LED);
-    apagado = al_create_bitmap(ANCHO_LED,ALTO_LED);
-    
-    if((!prendido)&&(!apagado))
+    for(cont_puerto=0;cont_puerto<CANT_PUERTOS_AUX;cont_puerto++)
     {
-        control = false;
-        al_destroy_bitmap(prendido);
-        al_destroy_bitmap(apagado);
-        
-    }else
-    {
-        //ALLEGRO_COLOR * fondo_color = al_get_pixel(al_get_backbuffer(display),0,0);
-        ALLEGRO_BITMAP * fondo = NULL;
-        fondo = al_create_bitmap(ANCHO_DIS,ALTO_LED);
-        
-        if(!fondo)
+        for(cont_leds=0;cont_leds<CANT_LEDS_PUERTO;cont_leds++)
         {
-            control = false;
-            al_destroy_bitmap(prendido);
-            al_destroy_bitmap(apagado);
-            al_destroy_bitmap(fondo);
-        } else
-        {
-           // al_set_target_bitmap(fondo);
-          //  al_clear_to_color(fondo_color);
-            
-            al_set_target_bitmap(prendido);
-            al_clear_to_color(al_color_name(PRENDIDO));
-
-            al_set_target_bitmap(apagado);
-            al_clear_to_color(al_color_name(APAGADO));
-
-            al_set_target_backbuffer(display);
-
-            al_draw_bitmap(fondo,0,0,0);
-            
-            for(cont_puerto=0;cont_puerto<CANT_PUERTOS_AUX;cont_puerto++)
-            {
-                for(cont_leds=0;cont_leds<CANT_LEDS_PUERTO;cont_leds++)
-                {
-                    estado = bitGet(cont_puerto*CANT_LEDS_PUERTO+cont_leds,datos,D);
-                    float posx = calc_posx_led(cont_leds,CANT_LEDS_PUERTO);
-                    float posy = calc_posy_led(cont_puerto,CANT_PUERTOS_AUX);
+            estado = bitGet(cont_puerto*CANT_LEDS_PUERTO+cont_leds,datos,D);
+            float posx = calc_posx_led(cont_leds,CANT_LEDS_PUERTO);
+            float posy = calc_posy_led(cont_puerto,CANT_PUERTOS_AUX);
                     
-                    if(estado == ON)
-                    {
-                        al_draw_bitmap(prendido,posx,posy,0);
-                    } else
-                    {
-                        al_draw_bitmap(apagado,posx,posy,0);
-                    }
-                   
-                }
+            if(estado == ON)
+            {
+                al_draw_bitmap(led_prendido,posx,posy,0);
+            } else
+            {
+                al_draw_bitmap(led_apagado,posx,posy,0);
             }
-            
-            al_flip_display();
-            al_destroy_bitmap(fondo);
-            al_destroy_bitmap(prendido);
-            al_destroy_bitmap(apagado);
-            
-            
+                   
         }
-        
-        
     }
-    
-    
-    return control;
-    
-}   
-
-
-bool crear_led(ALLEGRO_DISPLAY *display, ALLEGRO_BITMAP *led, float ancho, float alto, float posx, float posy)
-{
-    
-    bool control = true;
-    
-    led = al_create_bitmap(ancho,alto);
-    
-    if(!led)
-    {
-       control = false;
-       
-    } else
-    {
-        al_set_target_bitmap(led);
-        al_clear_to_color(al_color_name(APAGADO));
-        al_set_target_backbuffer(display);
-        al_draw_bitmap(led,posx,posy,0);
-        
-        
-    }
-    
-    return control;
-    
+            
+          
 }
-
+        
 
 float calc_posx_led (int nro_led, int tamano_puerto)
 {
